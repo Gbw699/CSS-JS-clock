6a4e651217d035d79e0d5e08433826b8326dc7a2
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-09-30 12:22:18 -0300

    aux

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const sec = document.querySelector(".sechand");
 const min = document.querySelector(".minhand");
 const hour = document.querySelector(".hourhand");
 
+let num = 0;
+let num1 = 0;
+let num2 = 0;
+
 function getDate() {
   const now = new Date();
   const seconds = now.getSeconds();
@@ -12,9 +16,19 @@ function getDate() {
   const minutesDegrees = (minutes / 60) * 360 + 90;
   const hoursDegrees = (hours / 24) * 360 + 90;
 
-  sec.style.transform = `rotate(${secondsDegrees}deg)`;
-  min.style.transform = `rotate(${minutesDegrees}deg)`;
-  hour.style.transform = `rotate(${hoursDegrees}deg)`;
+  if (seconds === 0) {
+    num += 360;
+  }
+  if (minutes === 0) {
+    num1 += 360;
+  }
+  if (hours === 0) {
+    num2 += 360;
+  }
+
+  sec.style.transform = `rotate(${num + secondsDegrees}deg)`;
+  min.style.transform = `rotate(${num1 + minutesDegrees}deg)`;
+  hour.style.transform = `rotate(${num2 + hoursDegrees}deg)`;
 }
 
 setInterval(getDate, 1000);
