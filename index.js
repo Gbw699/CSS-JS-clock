9961682272edbee410d9e22f81fcb008b4464aa8
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-09-30 01:57:41 -0300

    ready

---
+++ index.js
@@ -0,0 +1,21 @@
+const sec = document.querySelector(".sechand");
+const min = document.querySelector(".minhand");
+const hour = document.querySelector(".hourhand");
+
+function getDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  let secondsDegrees = (seconds / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const hoursDegrees = (hours / 24) * 360 + 90;
+
+  sec.style.transform = `rotate(${secondsDegrees}deg)`;
+  min.style.transform = `rotate(${minutesDegrees}deg)`;
+  hour.style.transform = `rotate(${hoursDegrees}deg)`;
+ 
+}
+
+setInterval(getDate, 1000);
